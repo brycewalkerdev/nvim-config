2df0466382f2c646d21513f0e35d8b1dc3e59f81
Bryce Walker <bwalker@soeffects.com>
2025-07-21 11:15:34 -0700

    push up to date

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -56,6 +56,7 @@ require('lazy').setup({
   require 'plugins/window-picker',
   require 'plugins/fugitive',
   require 'plugins/diffview',
+  require 'plugins/log-highlight',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
